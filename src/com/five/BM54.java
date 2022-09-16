d8d3d061696a4440727d6e173f3e4cc60659a171
liwuyi <1666071726@qq.com>
2022-09-16 14:34:39 +0800

    Initial commit

--- src/com/five/BM54.java
+++ src/com/five/BM54.java
@@ -13,7 +13,7 @@ public class BM54 {
     public ArrayList<ArrayList<Integer>> threeSum(int[] num) {
         //1.定义存放最后的结果集
         ArrayList<ArrayList<Integer>> lists=new ArrayList<>();
-        //保存三个数的值，用来判断是否符合题意left+middle+right=0；并且三元组中的值是按顺序递增的 保证有序
+        //保存三个数的值，用来判断是否符合题意left+middle+right=0；并且三元组中的值是按顺序递增的 保证元素有序
         int left,middle,right;
         //2.排序
         Arrays.sort(num);
@@ -24,17 +24,17 @@ public class BM54 {
         }
         //4.遍历数组进行判断
         for (int i = 0; i < num.length; i=hashMap.get(num[i])+1) {
-            //记录三元组第一个元素的值
+            //4.1 记录三元组第一个元素的值
             left=num[i];
             for(int j=i+1;j<num.length;j=hashMap.get(num[j])+1){
-                //记录三元组第二个元素的值
+                //4.2 记录三元组第二个元素的值
                 middle=num[j];
-                //三元组中第三个元素的值
+                //4.3 三元组中第三个元素的值
                 right=-left-middle;
                 //如果第三元素的值小于第二元素，直接跳出这次循环
                 if (right<middle)
                     break;
-                //如果第三元素的值在集合中存在，并且第三元素的下标在第二元素的之后，该三元组才是符合题意的
+                //4.4 如果第三元素的值在集合中存在，并且第三元素的下标在第二元素的之后，该三元组才是符合题意的
                 if (hashMap.get(right)!=null&&hashMap.get(right)>j){
                     ArrayList<Integer> arrayList=new ArrayList<>();
                     arrayList.add(left);
