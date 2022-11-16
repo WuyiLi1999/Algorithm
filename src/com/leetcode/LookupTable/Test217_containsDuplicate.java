469def9701fd73c9280c8ffd615b7105c7736433
liwuyi <1666071726@qq.com>
2022-11-16 10:01:18 +0800

    Initial commit

--- src/com/leetcode/LookupTable/Test217_containsDuplicate.java
+++ src/com/leetcode/LookupTable/Test217_containsDuplicate.java
@@ -1,4 +1,16 @@
 package com.leetcode.LookupTable;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Test217_containsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> hashSet=new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hashSet.contains(nums[i]))
+                return true;
+            hashSet.add(nums[i]);
+        }
+        return false;
+    }
 }
