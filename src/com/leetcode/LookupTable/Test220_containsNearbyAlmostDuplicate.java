7783f5de6c8e14f20ef3f1f9ba1a856ed08dcace
liwuyi <1666071726@qq.com>
2023-10-09 17:57:14 +0800

    220:存在重复元素三

--- src/com/leetcode/LookupTable/Test220_containsNearbyAlmostDuplicate.java
+++ src/com/leetcode/LookupTable/Test220_containsNearbyAlmostDuplicate.java
@@ -1,7 +1,5 @@
 package com.leetcode.LookupTable;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.TreeSet;
 
 //220:存在重复元素三
@@ -15,18 +13,19 @@ public class Test220_containsNearbyAlmostDuplicate {
         TreeSet<Long> treeSet=new TreeSet<>();
         for (int i = 0; i < nums.length; i++) {
             //如果当前集合中的元素个数等于滑动窗口中最多能存放的元素个数--移除滑动窗口的第一个元素
-            if (treeSet.size()==indexDiff+1)
+            if (treeSet.size()==indexDiff+1) {
                 treeSet.remove((long)nums[i-indexDiff-1]);
+            }
             //判断当前元素在滑动窗口中能否找到一个元素满足大于等于 num[i]-valueDiff  如果有就返回符合的一个最小值，没有返回null
-            Long num=treeSet.ceiling((long) nums[i]-(long)valueDiff);
+            Long num=treeSet.floor((long) nums[i]+(long)valueDiff);
             //如果这个最小值存在并且小于等于num[i]+valueDiff --证明在滑动窗口中该元素是符合题意的
-            if (num!=null&&num<=(long)nums[i]+(long) valueDiff)
+            if (num!=null&&num>=(long)nums[i]-(long) valueDiff) {
                 return true;
+            }
             treeSet.add((long)nums[i]);
         }
         return false;
     }
-
     public static void main(String[] args) {
         int [] nums={1,5,9,1,5,9};
         System.out.println(new Test220_containsNearbyAlmostDuplicate().containsNearbyAlmostDuplicate(nums,2,3)?1:0);
