2f865a50bd71ddb367cb25d6d20304922d9843f4
liwuyi <1666071726@qq.com>
2023-10-10 10:56:46 +0800

    删除有序链表中的重复元素

--- src/com/leetcode/LinkedList/Test82_deleteDuplicates.java
+++ src/com/leetcode/LinkedList/Test82_deleteDuplicates.java
@@ -1,25 +1,29 @@
 package com.leetcode.LinkedList;
 //82:删除排序链表中的重复元素（虚拟头结点）
 public class Test82_deleteDuplicates {
-
     public ListNode deleteDuplicates(ListNode head) {
-        if (head==null)
+        if (head==null){
             return null;
-        ListNode preHead=new ListNode(-1);
-        preHead.next=head;
-        ListNode curNode=preHead;
-        //如果当前节点的next与当前节点的next的next不为null ---  遍历
+        }
+        //虚拟头结点（防止头结点是重复元素，删除头结点之后导致头结点丢失）
+        ListNode dummyHead=new ListNode(-1);
+        dummyHead.next=head;
+        //当前节点指向虚拟头结点（遍历当前节点）
+        ListNode curNode=dummyHead;
+        //遍历当前节点（当前节点的next和当前节点的next的next不为null的时候）
         while (curNode.next!=null&&curNode.next.next!=null){
-            //如果当前节点的next与当前节点的next的next值相等--就删除当前节点的next的值，直到不相等
+            //如果当前节点的next和当前节点的next的next节点的相等——需要删除这些节点
             if (curNode.next.val==curNode.next.next.val){
-                int val=curNode.next.val;
+                int val = curNode.next.val;
+                //循环删除val相等的节点，当前节点的next的val值与记录的val值相等——就断开next节点与curNode的连接
                 while (curNode.next!=null&&curNode.next.val==val){
                     curNode.next=curNode.next.next;
                 }
-            }else {//不相等--继续判断下一个节点
+            }else {
                 curNode=curNode.next;
             }
         }
-        return preHead.next;
+        return dummyHead.next;
     }
+
 }
--- src/com/leetcode/LinkedList/Test83_deleteDuplicates.java
+++ src/com/leetcode/LinkedList/Test83_deleteDuplicates.java
@@ -3,30 +3,41 @@ package com.leetcode.LinkedList;
 //83:删除排序链表中的重复元素
 public class Test83_deleteDuplicates {
     public ListNode deleteDuplicates(ListNode head) {
-        if (head==null)
+        if (head==null) {
             return null;
+        }
+        //双指针（一个指向当前节点，一个指向与当前节点最近一次不相同的节点）
         ListNode curNode=head;
+        //处理完成之后的节点头部
         ListNode preNode=head;
+        //循环遍历当前节点  直到节点为null
         while (curNode!=null){
+            //如果当前节点与最近一个不相等的节点的值不相等，那么当前节点不需要删除，更新与当前节点最近一次不相同的节点值为当前节点
             if (preNode.val!=curNode.val){
                 preNode.next=curNode;
                 preNode=curNode;
             }
+            //当前节点指向当前节点的下一个节点
             curNode=curNode.next;
         }
+        //更新preNode节点的next指针
         preNode.next=null;
         return head;
     }
-    public ListNode deleteDuplicates_1(ListNode head) {
-        if (head==null)
-            return null;
-        ListNode curNode=head;
 
+    public ListNode deleteDuplicates_1(ListNode head) {
+        if (head==null){
+            return null;
+        }
+        ListNode curNode=head;
         while (curNode!=null&&curNode.next!=null){
-            if (curNode.val==curNode.next.val)
+            int val=curNode.val;
+            if (curNode.next.val==val){
                 curNode.next=curNode.next.next;
-            else
+            }else {
                 curNode=curNode.next;
+            }
+
         }
         return head;
     }
