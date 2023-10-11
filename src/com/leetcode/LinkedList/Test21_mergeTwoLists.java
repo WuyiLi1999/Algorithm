8987d7421a6f408d2660f6da3c6bee4f5404583d
liwuyi <1666071726@qq.com>
2023-10-11 17:04:00 +0800

    21:合并两个链表

--- src/com/leetcode/LinkedList/Test21_mergeTwoLists.java
+++ src/com/leetcode/LinkedList/Test21_mergeTwoLists.java
@@ -3,13 +3,11 @@ package com.leetcode.LinkedList;
 //21：合并两个有序链表
 public class Test21_mergeTwoLists {
     //迭代法
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list1==null||list2==null)
-            return list1==null?list2:list1;
-        ListNode preHead=new ListNode(-1);
-        ListNode curNode=preHead;
-        while (list1!=null||list2!=null){
-            if (list1.val <= list2.val){
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2){
+        ListNode dummyHead=new ListNode(-1);
+        ListNode curNode=dummyHead;
+        while (list1!=null&&list2!=null){
+            if (list1.val< list2.val){
                 curNode.next=list1;
                 list1=list1.next;
             }else {
@@ -18,17 +16,19 @@ public class Test21_mergeTwoLists {
             }
             curNode=curNode.next;
         }
-        if (list1!=null)
+        if (list1!=null){
             curNode.next=list1;
-        if (list2!=null)
+        }
+        if (list2!=null){
             curNode.next=list2;
-        return preHead.next;
+        }
+        return dummyHead.next;
     }
     //递归法
     public ListNode mergeTwoLists_1(ListNode list1, ListNode list2) {
-        if (list1==null||list2==null)
+        if (list1==null||list2==null) {
             return list1==null?list2:list1;
-        else if (list1.val<= list2.val){
+        } else if (list1.val<= list2.val){
             list1.next=mergeTwoLists_1(list1.next,list2);
             return list1;
         }else {
