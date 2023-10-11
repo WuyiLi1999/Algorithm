6b8d67975d5f9ef3dca3cc0b2d6de30f24c42ff1
liwuyi <1666071726@qq.com>
2023-10-11 19:22:59 +0800

    25：K个一组翻转链表

--- src/com/leetcode/LinkedList/Test25_reverseKGroup.java
+++ src/com/leetcode/LinkedList/Test25_reverseKGroup.java
@@ -1,41 +1,52 @@
 package com.leetcode.LinkedList;
 //25:K个一组翻转链表
 public class Test25_reverseKGroup {
-    public ListNode reverseKGroup(ListNode head, int k) {
-        if (head==null)
+
+    public ListNode reverseKGroup(ListNode head, int k){
+        if (head==null){
             return null;
-        //建立一个虚拟头结点
+        }
+        //虚拟头结点
         ListNode dummyHead=new ListNode(-1);
         dummyHead.next=head;
-        ListNode pre=dummyHead;//保存待翻转节点的前一个节点（已经反转好）
-        ListNode end=dummyHead;//保存每一次翻转的节点的末尾（下一次要翻转的头结点）
-        while (end.next!=null){
+        //保存待翻转节点的前一个节点（之前都是翻转好的）
+        ListNode preNode=dummyHead;
+        //保存每一次翻转的节点的末尾（下一次要翻转的头结点）
+        ListNode endNode=dummyHead;
+        while (endNode.next!=null){
             //找到待翻转链表的末尾结点（k个）
-            for (int i = 0; i < k && end!=null; i++) {
-                end=end.next;
+            for (int i = 0; endNode!=null&&i < k; i++) {
+                endNode = endNode.next;
             }
-            if (end==null)//待翻转的链表长度不足k个--不翻转
+            //如果不足k个就不翻转
+            if (endNode==null){
                 break;
-            //断开长度为 k 的链表
-            ListNode curNode=pre.next;//保存当前要反转的K长度的链表的头结点
-            ListNode next=end.next;//存接下来要翻转的长度为K的链表的头结点
-            end.next=null;//断开长度为 k 的链表
-            //翻转链表--返回翻转后的链表的头结点（赋值给pre.next）
-            pre.next=reverse(curNode);
-
-            curNode.next=next;//翻转之后curNode指向末尾结点，因此使用curNode与原链表的接下来要反转的节点连接起来
-            pre=curNode;//将反转之后的尾端赋值给下一次要翻转的长度为K的链表的头
-
-            end=pre;//end保存下一次要翻转的头结点
+            }
+            //记录要翻转的k个长度的链表
+            ///保存当前要反转的长度为k的链表的头部
+            ListNode reverseNode=preNode.next;
+            //记录下一次要翻转的链表的头结点
+            ListNode next=endNode.next;
+            endNode.next=null;
+            //翻转链表，并将翻转后的链表指向已经翻转好的节点的next指针
+            preNode.next=reverse(reverseNode);
+            //翻转之前的链表的头部（现在是链表的末尾）的next指向接下来要翻转的节点
+            reverseNode.next=next;
+            //接下来要翻转的节点的前一个节点指向翻转之前的链表的头部（现在是链表的末尾）
+            preNode=reverseNode;
+            //接下俩要翻转的链表的前一个节点为翻转之前的链表的头部（现在是链表的末尾）
+            endNode=preNode;
         }
         return dummyHead.next;
     }
-
     private ListNode reverse(ListNode curNode) {
-        ListNode pre=null;//已经反转的链表的头节点（初始为null）
-        ListNode cur=curNode;//要反转的当前节点
+        //已经反转的链表的头节点（初始为null）
+        ListNode pre=null;
+        //要反转的当前节点
+        ListNode cur=curNode;
         while (cur!=null){
-            ListNode node=cur.next;//保存接下俩要翻转的链表头结点
+            //保存接下俩要翻转的链表头结点
+            ListNode node=cur.next;
             cur.next=pre;
             pre=cur;
             cur=node;
