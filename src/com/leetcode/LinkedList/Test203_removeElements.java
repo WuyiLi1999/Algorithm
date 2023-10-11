860caacd037ab9f77625fe46f70efdd7474d3e4d
liwuyi <1666071726@qq.com>
2023-10-11 16:26:45 +0800

    203:删除指定链表元素

--- src/com/leetcode/LinkedList/Test203_removeElements.java
+++ src/com/leetcode/LinkedList/Test203_removeElements.java
@@ -2,22 +2,24 @@ package com.leetcode.LinkedList;
 
 //203:移除链表中指定值的元素(虚拟头结点)
 public class Test203_removeElements {
-    public ListNode removeElements(ListNode head, int val) {
-        if(head==null)
+
+    public ListNode removeElements(ListNode head, int val){
+        if (head==null){
             return null;
-        ListNode preHead=new ListNode(-1);
-        preHead.next=head;
-        ListNode curNode=preHead;
-        while (curNode.next!=null){
-            //找到节点值等于指定值的的节点--删除
-            if (curNode.next.val==val) {
-                ListNode delNode = curNode.next;
-                curNode.next = delNode.next;
-                delNode.next = null;
-            }
-            else
-                curNode=curNode.next;
         }
-        return preHead.next;
+        //虚拟头结点
+        ListNode dummyHead=new ListNode(-1);
+        dummyHead.next=head;
+        ListNode curNode = dummyHead;
+        //循环遍历当前节点的next  直到next=null
+        while (curNode.next!=null){
+            //判断当前节点的next节点的值是否的能与要删除的值，是的话，断开与原链表的连接
+            if (curNode.next.val ==val){
+                curNode.next=curNode.next.next;
+            }else {
+                curNode=curNode.next;
+            }
+        }
+        return dummyHead.next;
     }
 }
