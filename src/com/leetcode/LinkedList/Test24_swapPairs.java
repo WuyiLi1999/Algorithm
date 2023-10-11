52185fd3e34d5e456c114a6191f6dccbe82a6f88
liwuyi <1666071726@qq.com>
2023-10-11 17:23:24 +0800

    24：两两交换链表中的节点

--- src/com/leetcode/LinkedList/Test24_swapPairs.java
+++ src/com/leetcode/LinkedList/Test24_swapPairs.java
@@ -2,29 +2,28 @@ package com.leetcode.LinkedList;
 
 //24:两两交换链表中的节点
 public class Test24_swapPairs {
-    //
-    public ListNode swapPairs(ListNode head) {
-        if(head==null)
-            return null;
-        //建立一个虚拟头结点
-        ListNode preHead=new ListNode(-1);
-        preHead.next=head;
-        ListNode curNode=preHead;
-        //设立一个新的节点对curNode节点操作就是对preHead点操作
-        // 在操作的过程中curNode的节点位置在变化，preHead一直指向链表的头结点）
-        while (curNode.next!=null&&curNode.next.next!=null){
-            //记录三个节点值（要交换的两个节点和下一个要交换的节点）
-            ListNode node1=curNode.next;
-            ListNode node2=node1.next;
-            ListNode next=node2.next;
-            //节点交换
-            node1.next=next;
-            node2.next=node1;
-            curNode.next=node2;
-            //curNode需要跳到第二个节点位置：在原来的时候curNode需要指向node2节点的，由于node1node2位置交换了
-            curNode=node1;
 
+    public ListNode swapPairs(ListNode head){
+        if (head==null){
+            return null;
         }
-        return preHead.next;
+        //建立一个虚拟头结点
+        ListNode dummyHead=new ListNode(-1);
+        dummyHead.next=head;
+        ListNode curNode=dummyHead;
+        //循环遍历当前节点（当前节点的next与当前节点的next的next都不为null--两两一组交换位置）
+        while (curNode.next!=null&&curNode.next.next!=null){
+            //记录当前要交换的两个元素与接下来要判断的元素
+            ListNode first=curNode.next;
+            ListNode second=first.next;
+            ListNode next= second.next;
+            // 节点交换
+            first.next=next;
+            curNode.next=second;
+            curNode.next.next=first;
+            //curNode需要跳到第二个节点位置：在原来的时候curNode需要指向node2节点的，由于node1node2位置交换了
+            curNode=first;
+        }
+        return dummyHead.next;
     }
 }
